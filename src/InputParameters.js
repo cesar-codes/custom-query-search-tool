fa1631b446ffd206d3237d79359123c9e3965e3d
cesar-codes <cesar.avina@gmail.com>
2023-02-28 16:18:32 -0500

    update react dependencies

--- src/InputParameters.js
+++ src/InputParameters.js
@@ -1,16 +1,44 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container, Dropdown, Grid, Segment } from "semantic-ui-react";
+import data from './RestaurantData.json'
+// const getData = () => {
+//     fetch('RestaurantData.json'
+//     ,{
+//         headers : { 
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//        }
+//     }
+//     )
+//     .then(function(response){
+//         console.log(response)
+//         return response.json();
+//       })
+//       .then(function(myJson) {
+//         console.log(myJson);
+//       });
+//   }
+//   useEffect(()=>{
+//     getData()
+//   },[])
+
+
+
 
 
 function InputParameters() {
-    const options = [
-          { key: 1, text: 'Restaurant 1', value: 1 },
-          { key: 2, text: 'Restaurant 2', value: 2 }
+    const resaurantData = data.map( (data) =>{
+    console.log(data)
+}
+)
+    const options = [resaurantData
+        //   { key: 1, text: 'Restaurant 1', value: 1 },
+        //   { key: 2, text: 'Restaurant 2', value: 2 }
     ];
 
     const [restaurantIds, setRestaurantIds] = useState([]);
-
+    console.log(data)
     return (
         <Grid>
             <Grid.Row columns={1}>
@@ -24,6 +52,9 @@ function InputParameters() {
                                     </Grid.Column>
                                 </Grid.Row>
                                 <Grid.Row>
+                                    <Grid.Column textAlign="center">
+                                        <h3>Restaurant</h3>
+                                    </Grid.Column>
                                     <Dropdown
                                         placeholder='Select restaurant...'
                                         fluid
@@ -34,6 +65,52 @@ function InputParameters() {
                                         onChange={(e, data) => setRestaurantIds(data.value)}
                                     />
                                 </Grid.Row>
+
+                                <Grid.Row>
+                                    <Grid.Column textAlign="center">
+                                        <h3>Time</h3>
+                                    </Grid.Column>
+                                    <Dropdown
+                                        placeholder='Select time range...'
+                                        fluid
+                                        multiple
+                                        selection
+                                        options={options}
+                                        value={restaurantIds}
+                                        onChange={(e, data) => setRestaurantIds(data.value)}
+                                    />
+                                </Grid.Row>
+
+                                <Grid.Row>
+                                    <Grid.Column textAlign="center">
+                                        <h3>Metrics</h3>
+                                    </Grid.Column>
+                                    <Dropdown
+                                        placeholder='Select metrics...'
+                                        fluid
+                                        multiple
+                                        selection
+                                        options={options}
+                                        value={restaurantIds}
+                                        onChange={(e, data) => setRestaurantIds(data.value)}
+                                    />
+                                </Grid.Row>
+
+                                <Grid.Row>
+                                    <Grid.Column textAlign="center">
+                                        <h3>Dates</h3>
+                                    </Grid.Column>
+                                    <Dropdown
+                                        placeholder='Select date range...'
+                                        fluid
+                                        multiple
+                                        selection
+                                        options={options}
+                                        value={restaurantIds}
+                                        onChange={(e, data) => setRestaurantIds(data.value)}
+                                    />
+                                </Grid.Row>
+
                             </Grid>
                         </Segment>
                     </Container>
