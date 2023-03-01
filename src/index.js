97b470eab7e903a2cc17f42b36507c27069dc696
cesar-codes <cesar.avina@gmail.com>
2023-03-01 11:10:29 -0500

    configure react-dates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'semantic-ui-css/semantic.min.css'
+import 'react-dates/initialize';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
