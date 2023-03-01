e1a573d9bbc451d27fe62345a61f52565399502f
cesar-codes <cesar.avina@gmail.com>
2023-03-01 12:21:01 -0500

    configure metric definitions and create API connection

--- src/InputParameters.js
+++ src/InputParameters.js
@@ -5,18 +5,54 @@ import { Container, Dropdown, Grid, Segment } from "semantic-ui-react";
 import moment from "moment";
 import data from './RestaurantData.json';
 
+async function getData(url = "") {
+    const response = await fetch(url, {
+        method: "GET",
+        cache: "no-cache"
+    });
+    return response.json();
+}
+
 function InputParameters() {
     const resaurantData = data.map( (data) =>{
-    console.log(data)
+    //console.log(data)
 }
 )
     const options = [resaurantData
         //   { key: 1, text: 'Restaurant 1', value: 1 },
         //   { key: 2, text: 'Restaurant 2', value: 2 }
     ];
-
+    
     const [restaurantIds, setRestaurantIds] = useState([]);
-    console.log(data)
+    const [startDate, setStartDate] = useState(moment("3-1-2023"));
+    const [endDate, setEndDate] = useState(moment("3-21-20203"));
+    const [focusedInput, setFocusedInput] = useState(null);
+    const [metricDefinitions, setMetricDefinitions] = useState([]);
+
+    console.log("Start Date", startDate)
+    console.log("End Date", endDate)
+
+    useEffect(() => {
+        getData("https://customsearchquerytoolapi.azurewebsites.net/Search/MetricDefinitions")
+        .then(data => {
+            setMetricDefinitions(data)
+        })
+    }, []);
+
+    console.log("metricDefinitions:", metricDefinitions)
+
+    const metricOptions = metricDefinitions.map( m => {
+        return ({
+            key: m.metricCode,
+            value: m.metricCode,
+            text: m.alias
+        })
+    });
+
+
+
+    //const [restaurantIds, setRestaurantIds] = useState([]);
+    //console.log(data)
     return (
         <Grid>
             <Grid.Row columns={1}>
