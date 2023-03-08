8db604b18d214b6de80a2780ff8fdacb851defde
cesar-codes <cesar.avina@gmail.com>
2023-03-08 10:26:31 -0500

    configure To and From dropdown times

--- src/InputParameters.js
+++ src/InputParameters.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { DateRangePicker } from "react-dates";
-import { Container, Dropdown, Grid, Segment } from "semantic-ui-react";
+import { Container, Dropdown, Grid, Segment, Input } from "semantic-ui-react";
 import moment from "moment";
-import data from './RestaurantData.json';
+import RestaurantData from './RestaurantData.json';
 
 async function getData(url = "") {
     const response = await fetch(url, {
@@ -13,24 +13,66 @@ async function getData(url = "") {
     return response.json();
 }
 
+
+
 function InputParameters() {
-    const restaurantData = data.map( (data) =>{
-    //console.log(data)
+    const restaurantData = RestaurantData.map( d =>{
+    return ({
+        key: d.Id,
+        value: d.Name,
+        text: d.Name
+    })
 }
 )
-    const options = [restaurantData
-        //   { key: 1, text: 'Restaurant 1', value: 1 },
-        //   { key: 2, text: 'Restaurant 2', value: 2 }
-    ];
-    
+console.log('data', RestaurantData)
+
+    // const options = [restaurantData
+    //     //   { key: 1, text: 'Restaurant 1', value: 1 },
+    //     //   { key: 2, text: 'Restaurant 2', value: 2 }
+    // ];
+    const timeOptions = [
+        {key: 1, text: '5 a.m.', value: 5},
+        {key: 2, text: '6 a.m.', value: 6},
+        {key: 3, text: '7 a.m.', value: 7},
+        {key: 4, text: '8 a.m.', value: 8},
+        {key: 5, text: '9 a.m.', value: 9},
+        {key: 6, text: '10 a.m.', value: 10},
+        {key: 7, text: '11 a.m.', value: 11},
+        {key: 8, text: '12 p.m.', value: 12},
+        {key: 9, text: '1 p.m.', value: 13},
+        {key: 10, text: '2 p.m.', value: 14},
+        {key: 11, text: '3 p.m.', value: 15},
+        {key: 12, text: '4 p.m.', value: 16},
+        {key: 13, text: '5 p.m.', value: 17},
+        {key: 14, text: '6 p.m.', value: 18},
+        {key: 15, text: '7 p.m.', value: 19},
+        {key: 16, text: '8 p.m.', value: 20},
+        {key: 17, text: '9 p.m.', value: 21},
+        {key: 18, text: '10 p.m.', value: 22},
+        {key: 19, text: '11 p.m.', value: 23},
+        {key: 20, text: '12 a.m.', value: 24},
+        {key: 21, text: '1 a.m.', value: 25},
+        {key: 22, text: '2 a.m.', value: 26},
+        {key: 23, text: '3 a.m.', value: 27},
+        {key: 23, text: '4 a.m.', value: 28},
+        {key: 24, text: '5 a.m. next day', value: 29},
+        {key: 25, text: '6 a.m. next day', value: 30},
+        {key: 26, text: '7 a.m. next day', value: 31},
+    ]
+
+    const [fromTime, setFromTime] = useState(5);
+    const [toTime, setToTime] = useState(29)
+
+    const [metric, setMetric] = useState(null)
+
     const [restaurantIds, setRestaurantIds] = useState([]);
     const [startDate, setStartDate] = useState(moment("3-1-2023"));
     const [endDate, setEndDate] = useState(moment("3-21-2023"));
     const [focusedInput, setFocusedInput] = useState(null);
     const [metricDefinitions, setMetricDefinitions] = useState([]);
 
-    console.log("Start Date", startDate)
-    console.log("End Date", endDate)
+    // console.log("Start Date", startDate)
+    // console.log("End Date", endDate)
 
     useEffect(() => {
         getData("https://customsearchquerytoolapi.azurewebsites.net/Search/MetricDefinitions")
@@ -39,7 +81,14 @@ function InputParameters() {
         })
     }, []);
 
-    console.log("metricDefinitions:", metricDefinitions)
+    //console.log("metricDefinitions:", metricDefinitions)
+   // console.log('restaurantData', restaurantData)
+
+    // const restaurantOptions = restaurantIds.map( m => {
+    //     return ({
+    //         key: 
+    //     })
+    // })
 
     const metricOptions = metricDefinitions.map( m => {
         return ({
@@ -48,7 +97,6 @@ function InputParameters() {
             text: m.alias
         })
     });
-
 
     return (
         <Grid>
@@ -72,32 +120,44 @@ function InputParameters() {
                                         fluid
                                         multiple
                                         selection
-                                        options={metricOptions}
-                                        //options={restaurantData}
+                                        options={restaurantData}
                                         value={restaurantIds}
                                         onChange={(e, data) => setRestaurantIds(data.value)}
                                     />
                                 </Grid.Row>
                                
                                 {/* Time Range picker */}
-                                <Grid.Row>
-                                    {/* <Grid.Column textAlign="center">
-                                        <h3>Dates></h3>
-                                    </Grid.Column> */}
-                                    <DateRangePicker
-                                        startDate={startDate}
-                                        startDateId="your_unique_start_date_id"
-                                        endDate={endDate}
-                                        endDateId="your_unique_end_date_id"
-                                        onDatesChange={({ startDate, endDate }) => {
-                                            setStartDate(startDate);
-                                            setEndDate(endDate)
-                                        }
-                                        }
-                                        focusedInput={focusedInput}
-                                        onFocusChange={ focusedInput => setFocusedInput(focusedInput)}
+                                <Grid.Row columns={2}>
+                                    <Grid.Column textAlign="center">
+                                            <h3>From</h3>
+                                            <Dropdown
+                                            placeholder='Start time...'
+                                            fluid
+                                            multiple
+                                            selection
+                                            options={timeOptions}
+                                            //options={restaurantData}
+                                            value={fromTime}
+                                            onChange={(e, data) => setFromTime(data.value)}
+                                        />
+                                    </Grid.Column>
+
+                                    <Grid.Column textAlign="center">
+                                        <h3>To</h3>
+                                        <Dropdown
+                                        placeholder='End time...'
+                                        fluid
+                                        multiple
+                                        selection
+                                        options={RestaurantData}
+                                        //options={restaurantData}
+                                        value={RestaurantData}
+                                        onChange={(e, data) => setRestaurantIds(data.value)}
                                     />
+                                    </Grid.Column>
                                 </Grid.Row>
+
+
 
                                 {/* Metrics */}
                                 <Grid.Row>
@@ -109,8 +169,8 @@ function InputParameters() {
                                         fluid
                                         selection
                                         options={metricOptions}
-                                        // value={restaurantIds}
-                                        // onChange={(e, data) => setRestaurantIds(data.value)}
+                                        value={metric}
+                                        onChange={(e, data) => setMetric(data.value)}
                                     />
                                 </Grid.Row>
 
